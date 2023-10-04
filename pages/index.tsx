2017a11f87354c9e44d4d2639219f50b193d685e
ChuYong <jakgon@naver.com>
2023-10-05 00:13:14 +0900

    feat: add profile pictures

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import '../app/global.css';
+import Image from "next/image";
+import { UserCircle } from 'iconoir-react';
 
 export default function Index() {
   return <div style={{
@@ -6,39 +8,42 @@ export default function Index() {
     flexDirection: 'row'
   }}>
     <Sidebar/>
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      color: '#ffffff',
-    }}>
-      <div className="w-40 h-2 bg-white"/>
-      <div className="h-2"/>
-      <span className="text-6xl">Welcome To</span>
-      <span className="text-7xl">YEONGMIN SONG</span>
-      <div className="h-3"/>
-      <span className="text-5xl" style={{fontFamily: 'Pretendard-Regular'}}>SERVER & INFRA ENGINEER</span>
-      <div className="h-11"/>
-      <div className="flex flex-row gap-4">
-        <Button text={"PROJECTS"}/>
-        <Button text={"CAREER"}/>
+    <CopyRight/>
+    <div className="flex flex-row justify-evenly items-center grow">
+      <div className="flex flex-col text-slate-50">
+        <div className="w-40 h-2 bg-white"/>
+        <div className="h-2"/>
+        <span className="text-6xl">Welcome To</span>
+        <span className="text-7xl">YEONGMIN SONG</span>
+        <div className="h-3"/>
+        <span className="text-5xl font-regular">SERVER & INFRA ENGINEER</span>
+        <div className="h-11"/>
+        <div className="flex flex-row gap-4">
+          <Button text={"PROJECTS"}/>
+          <Button text={"CAREER"}/>
+        </div>
+      </div>
+      <div>
+        <Image src={"/yeongmin.png"} width={300} height={300} className="w-[28rem] h-[28rem]"/>
       </div>
     </div>
+  <SlideToNext/>
+
   </div>
 }
 
 function Sidebar() {
-  return <div style={{
-    width: '5.8%',
-    height: '100vh',
-    backgroundColor: '#111111'
-  }}>
+  return <div className="h-screen w-24 bg-stone-900">
+    <div className="flex flex-col items-center">
+      <UserCircle className="text-slate-50 w-10 h-10"/>
+    </div>
 
   </div>
 }
 
 function Button({text}: {text: string}) {
   return <div
-    className="w-fit"
+    className="w-fit button-box"
     style={{
     borderColor: '#BFDBFE',
     borderWidth: '2.3px',
@@ -47,5 +52,17 @@ function Button({text}: {text: string}) {
       fontFamily: 'Pretendard-Regular',
   }}>
     {text}
+  </div>
+}
+
+function CopyRight() {
+  return <div className="absolute top-0 right-0 text-slate-50 pr-10 pt-5">
+    <span>ⓒ 2023 CChuYong</span>
+  </div>
+}
+
+function SlideToNext() {
+  return <div className="absolute bottom-0 right-0 text-slate-50 pr-10 pb-10 text-1xl [writing-mode:vertical-lr]">
+    <span>SLIDE TO NEXT -></span>
   </div>
 }
