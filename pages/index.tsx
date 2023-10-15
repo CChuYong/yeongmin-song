534015bfb02d3a90717bec1917825da08ee01ff8
송영민 <jakgon@naver.com>
2023-10-15 21:26:32 +0900

    feat: add temporary portfolio url

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,40 +7,26 @@ export default function Index() {
         flexDirection: 'row',
         height: '100vh'
     }}>
-        <div className="flex flex-row justify-center items-center grow gap-20">
-            <span className="text-8xl">🛠️</span>
+        <div className="flex flex-row justify-center items-center grow gap-36">
+            <span className="text-9xl">🛠️</span>
             <div className="flex flex-col text-slate-50">
                 <span className="text-7xl">Under Construction</span>
                 <span className="text-5xl font-regular">Check Back Later!</span>
+                <div className="h-11"/>
+                <Button text={"Go To Temporary Portfolio Page"} onTap={() => location.href = 'https://about.yeongmin.kr/'}/>
             </div>
         </div>
-    </div>
-}
-
-function Sidebar() {
-    return <div className="h-screen w-20 bg-stone-900">
-        <div className="flex flex-col justify-between items-center py-5 h-screen">
-            <div className="flex flex-col items-center gap-5">
-                <UserCircle className="text-slate-50 w-10 h-10"/>
-                <Building className="text-slate-50 h-11 w-14"/>
-                <Folder className="text-slate-50 w-10 h-10"/>
-            </div>
-            <div className="flex flex-col items-center gap-5">
-                <Instagram className="text-slate-50 w-10 h-10"/>
-                <LinkedIn className="text-slate-50 h-11 w-14"/>
-                <GithubCircle className="text-slate-50 w-10 h-10"/>
-            </div>
-        </div>
-
     </div>
 }
 
 interface ButtonProps {
-    text: string
+    text: string,
+    onTap?: () => void,
 }
 
-function Button({text}: ButtonProps) {
+function Button({text, onTap}: ButtonProps) {
     return <div
+        onClick={onTap}
         className="w-fit button-box"
         style={{
             borderColor: '#BFDBFE',
@@ -50,17 +36,5 @@ function Button({text}: ButtonProps) {
             fontFamily: 'Pretendard-Regular',
         }}>
         {text}
-    </div>
-}
-
-function CopyRight() {
-    return <div className="absolute top-0 right-0 text-slate-50 pr-10 pt-5">
-        <span>ⓒ 2023 CChuYong</span>
-    </div>
-}
-
-function SlideToNext() {
-    return <div className="absolute bottom-0 right-0 text-slate-50 pr-10 pb-10 text-1xl [writing-mode:vertical-lr]">
-        <span>SLIDE TO NEXT &gt;</span>
     </div>
 }
