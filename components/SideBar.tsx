844551a0d9818436518ed1f900737f6dc2a08af7
ChuYong <jakgon@naver.com>
2023-11-01 12:30:35 +0900

    feat: 버튼 호버 이벤트 추가

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,9 +6,9 @@ export default function Sidebar() {
   return <div className="h-screen w-20 bg-stone-900">
     <div className="flex flex-col justify-between items-center py-5 h-screen">
       <div className="flex flex-col items-center gap-5">
-        <UserCircle className="text-slate-50 w-10 h-10"/>
-        <Building className="text-slate-50 h-11 w-14"/>
-        <Folder className="text-slate-50 w-10 h-10"/>
+        <UserCircle className="text-slate-50 w-10 h-10 hover:cursor-pointer hover:scale-90"/>
+        <Building className="text-slate-50 h-11 w-14 hover:cursor-pointer hover:scale-90"/>
+        <Folder className="text-slate-50 w-10 h-10 hover:cursor-pointer hover:scale-90"/>
       </div>
       <div className="flex flex-col items-center gap-5">
         <Instagram onClick={() => onExternalLink(SideBarExternalLink.INSTAGRAM)} className="text-slate-50 w-10 h-10 hover:cursor-pointer hover:scale-90"/>
